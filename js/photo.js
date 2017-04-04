4f455d7f4584d60284821fb97d0b035411cf734f
joyyzhang <joyyzhang>
2017-04-04 19:08:51 +0800

    Site updated: 2017-04-04 19:08:51

--- js/photo.js
+++ js/photo.js
@@ -1,7 +1,7 @@
 $(function(){
 
     var pages='<h1>宜昌·三峡</h1><br/><br/><p></p>'+
-			'<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/1.jpg"></a><img class="img-img1" src="/photos/1.jpg" /></div>'+
+			'<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io/blob/master/photos/1.jpg"></a><img class="img-img1" src="/photos/1.jpg" /></div>'+
         	'<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/2.JPG"></a><img class="img-img1" src="/photos/2.jpg" /></div>'+
             '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/3.jpg"></a><img class="img-img1" src="/photos/3.jpg" /></div>'+
             '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/4.jpg"></a><img class="img-img1" src="/photos/4.jpg" /></div>'+
