4eafa511fb286dc570add09c8c485d4dd95304c6
joyyzhang <joyyzhang>
2017-04-04 19:29:35 +0800

    Site updated: 2017-04-04 19:29:34

--- js/photo.js
+++ js/photo.js
@@ -1,7 +1,7 @@
 $(function(){
 
-    var pages='<h1>宜昌·三峡</h1><br/><br/><p></p>'+
-    		'<ul>'+
+    var pages='<ul>'+
+    		'<h1>宜昌·三峡</h1><br/><br/><p></p>'+
 			'<li style="display:inline-block;list-style:none"><div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io/blob/master/photos/1.jpg"></a><img class="img-img1" src="/photos/1.jpg" /></div></li>'+
         	'<li style="display:inline-block;list-style:none"><div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/2.JPG"></a><img class="img-img1" src="/photos/2.jpg" /></div></li>'+
             '<li style="display:inline-block;list-style:none"><div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/3.jpg"></a><img class="img-img1" src="/photos/3.jpg" /></div></li>'+
@@ -12,12 +12,4 @@ $(function(){
             '<li style="display:inline-block;list-style:none"><div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/4.jpg"></a><img class="img-img1" src="/photos/4.jpg" /></div></li>'+
             '</ul>';
     $("#myimg").html(pages);
-    
-
-
-	var pin_height = $(".img-box").width(); //   获取div的宽度
-    $(".img-box").css('height', pin_height);  //  设置div  高度
-    $(".img-img1").css('width', pin_height);  //  设置div  高度
-    $(".img-img1").css('height', pin_height);  //  设置div  
-
 });
