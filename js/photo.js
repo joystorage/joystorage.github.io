3bea89200b58f1280bcea188dba55a8aae6e0828
joyyzhang <joyyzhang>
2017-04-04 19:47:26 +0800

    Site updated: 2017-04-04 19:47:26

--- js/photo.js
+++ js/photo.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    var pages='<ul style="list-style:none">'+
+    var pages='<ul class="photo_ul">'+
     		'<h1>宜昌·三峡</h1><br/><br/><p></p>'+
 			'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io/blob/master/photos/1.jpg"></a><img class="img-img1" src="/photos/1.jpg" /></div></li>'+
         	'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/2.JPG"></a><img class="img-img1" src="/photos/2.jpg" /></div></li>'+
@@ -13,7 +13,8 @@ $(function(){
             '</ul>';
     $("#myphoto").html(pages);
 
-    var pin_height = $(".img-box").width(); //   获取div的宽度
+    var pin_height = $(".photo_li").width(); //   获取div的宽度
+    $(".photo_li").css('height', pin_height);  //  设置div 
     $(".img-box").css('height', pin_height);  //  设置div  高度
     $(".img-img1").css('width', pin_height);  //  设置div  高度
     $(".img-img1").css('height', pin_height);  //  设置div 
