73777aaf5ae6ec7eaea5dd63f1bc32e2401a6829
joyyzhang <joyyzhang>
2017-04-04 18:54:14 +0800

    Site updated: 2017-04-04 18:54:14

--- js/photo.js
+++ js/photo.js
@@ -5,9 +5,11 @@ $(function(){
         	'<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/2.JPG"></a><img class="img-img1" src="/photos/2.jpg" /></div>'+
             '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/3.jpg"></a><img class="img-img1" src="/photos/3.jpg" /></div>'+
             '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/4.jpg"></a><img class="img-img1" src="/photos/4.jpg" /></div>'+
-            '<br/><br/><br/><div style="height:30px"></div>';
+            '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/4.jpg"></a><img class="img-img1" src="/photos/4.jpg" /></div>'+
+            '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/4.jpg"></a><img class="img-img1" src="/photos/4.jpg" /></div>'+
+            '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/4.jpg"></a><img class="img-img1" src="/photos/4.jpg" /></div>'+
+            '<br/><br/><br/><hr sytle="width:20px;color:gray"/>';
     $("#myimg_1").html(pages);
-    $("#myimg_2").html(pages);
     
 
 
