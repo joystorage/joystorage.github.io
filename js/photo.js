9ba1bf1fe49db116aed208548613d3c12600ae25
joyyzhang <joyyzhang>
2017-04-04 18:42:22 +0800

    Site updated: 2017-04-04 18:42:22

--- js/photo.js
+++ js/photo.js
@@ -1,10 +1,11 @@
 $(function(){
 
-    var pages='<h1>宜昌·三峡</h1><br>'+
+    var pages='<h1>宜昌·三峡</h1><br><br>'+
 			'<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/1.jpg"></a><img class="img-img1" src="/photos/1.jpg" /></div>'+
         	'<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/2.JPG"></a><img class="img-img1" src="/photos/2.jpg" /></div>'+
             '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/3.jpg"></a><img class="img-img1" src="/photos/3.jpg" /></div>'+
-            '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/4.jpg"></a><img class="img-img1" src="/photos/4.jpg" /></div>';
+            '<div class="img-box"><a class="img-bg" rel="example_group" href="https://github.com/joystorage/joystorage.github.io.git/photos/4.jpg"></a><img class="img-img1" src="/photos/4.jpg" /></div>'+
+            '<br><br>';
     $("#myimg_1").html(pages);
     $("#myimg_2").html(pages);
     
