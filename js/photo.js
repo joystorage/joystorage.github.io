042486105d02c7cd3424c930afd84062d180ba78
joyyzhang <joyyzhang>
2017-04-04 21:32:56 +0800

    Site updated: 2017-04-04 21:32:56

--- js/photo.js
+++ js/photo.js
@@ -11,35 +11,36 @@ $(function(){
 
      var pages='<ul class="photo_ul">'+
     		'<h1>湖北·宜昌</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/1.jpg"><img class="img-img1" src="/photos/1.jpg" /></a></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/2.jpg"><img class="img-img1" src="/photos/2.jpg" /></a></div></li>'+
-            '<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/3.jpg"><img class="img-img1" src="/photos/3.jpg" /></a></div></li>'+
-            '<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/4.jpg"><img class="img-img1" src="/photos/4.jpg" /></a></div></li>'+
+			'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/1.jpg"><img class="img-img1" lazy-src="/photos/1.jpg" /></a></div></li>'+
+        	'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/2.jpg"><img class="img-img1" lazy-src="/photos/2.jpg" /></a></div></li>'+
+            '<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/3.jpg"><img class="img-img1" lazy-src="/photos/3.jpg" /></a></div></li>'+
+            '<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/4.jpg"><img class="img-img1" lazy-src="/photos/4.jpg" /></a></div></li>'+
             '</ul><br/>';
     $("#myphoto").append(pages);
 
      var pages='<ul class="photo_ul">'+
     		'<h1>北京</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><img class="img-img1" src="/photos/北京1.jpg" /></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1" src="/photos/北京2.jpg" /></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1" src="/photos/北京3.jpg" /></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1" src="/photos/北京4.jpg" /></div></li>'+
+			'<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/北京1.jpg" /></div></li>'+
+        	'<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/北京2.jpg" /></div></li>'+
+            '<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/北京3.jpg" /></div></li>'+
+            '<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/北京4.jpg" /></div></li>'+
             '</ul><br/>';
     $("#myphoto").append(pages);
 
      var pages='<ul class="photo_ul">'+
     		'<h1>广东·深圳</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/深圳1.jpg"><img class="img-img1" src="/photos/深圳1.jpg" /></a></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/深圳2.jpg"><img class="img-img1" src="/photos/深圳2.jpg" /></a></div></li>'+
-            '<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/深圳3.jpg"><img class="img-img1" src="/photos/深圳3.jpg" /></a></div></li>'+
-            '<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/深圳4.jpg"><img class="img-img1" src="/photos/深圳4.jpg" /></a></div></li>'+
+			'<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/深圳1.jpg" /></div></li>'+
+        	'<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/深圳2.jpg" /></div></li>'+
+            '<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/深圳3.jpg" /></div></li>'+
+            '<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/深圳4.jpg" /></div></li>'+
             '</ul><br/><br/><br/>';
     $("#myphoto").append(pages);
 
      var pages='<ul class="photo_ul">'+
     		'<h1>吉林·长春</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/长春1.jpg"><img class="img-img1" src="/photos/长春1.jpg" /></a></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="/photos/长春2.jpg"><img class="img-img1" src="/photos/长春2.jpg" /></a></div></li>'+
+			'<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/长春1.jpg" /></div></li>'+
+        	'<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/长春2.jpg" /></div></li>'+
+        	'<li class="photo_li"><div class="img-box"><img class="img-img1" lazy-src="/photos/长春3.jpg" /></div></li>'+
             '</ul><br/><br/><br/>';
     $("#myphoto").append(pages);
 
