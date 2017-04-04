a22c5a4673e7a1b4e1b7cd0b67ebae28ee33a68a
joyyzhang <joyyzhang>
2017-04-04 18:18:50 +0800

    Site updated: 2017-04-04 18:18:50

--- js/photo.js
+++ js/photo.js
@@ -2,4 +2,6 @@ $(function(){
 	console.log("asdfasdfasdf");
 	var pin_height = $(".img-box").width(); //   获取div的宽度
     $(".img-box").css('height', pin_height);  //  设置div  高度
+    $(".img-img1").css('width', pin_height);  //  设置div  高度
+    $(".img-img1").css('height', pin_height);  //  设置div  高度
 });
