46525ffe0c2b0fb9c5f4cb7179c32fd530bd493c
joyyzhang <joyyzhang>
2017-04-04 18:02:05 +0800

    Site updated: 2017-04-04 18:02:04

--- js/photo.js
+++ js/photo.js
@@ -2,5 +2,4 @@ $(function(){
 	console.log("asdfasdfasdf");
 	var pin_height = $(".img-box").width(); //   获取div的宽度
     $(".img-box").css('height', pin_height);  //  设置div  高度
-    $(".img-img1").css('height', pin_height);  //  设置div  高度
 });
