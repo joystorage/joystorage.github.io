20f71acfb6a75cae489b4f25b8b7bc3f759b2f01
joyyzhang <joyyzhang>
2017-04-05 12:28:38 +0800

    Site updated: 2017-04-05 12:28:37

--- js/photo.js
+++ js/photo.js
@@ -1,70 +1,27 @@
 $(function(){
+	$("#myphoto").html('');
+
     $.getJSON("/json/photos.json",function(data) {
     	var rootpath=data.root;
+    	var pages = '';
         $.each(data.contents, function(i, item) {
         	var title = item.title;
-        	console.log("123");
-        	console.log(title);
+
+        	pages = '<ul class="photo_ul">';
+        	pages += '<h1>'+title+'</h1><br/><br/><p></p>';
+
         	var secpath = rootpath + item.pre;
-        	console.log(secpath);
         	$.each(item.data,function(j,item2){
-            	console.log(secpath+item2.url);
+            	var con = secpath+item2.url;
+            	pages += '<li class="photo_li">'+
+            			'<div class="img-box"><a href="'+con+'"></a>'+
+            			'<img class="img-img1"  data-url="/photos/timg.jpg" src="'+con+'" /></div></li>';
         	});
+
+        	pages += '</ul><br/>';
+        	$("#myphoto").append(pages);
     	});
     });
-
-    var pages='<ul class="photo_ul">'+
-    		'<h1>黑龙江·哈尔滨</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="http://onx1obrfu.bkt.clouddn.com/joystorage/photos/哈尔滨1.jpg"></a><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg" src="http://onx1obrfu.bkt.clouddn.com/joystorage/photos/哈尔滨1.jpg" /></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg" src="http://onx1obrfu.bkt.clouddn.com/joystorage/photos/哈尔滨2.jpg" /></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg" src="http://onx1obrfu.bkt.clouddn.com/joystorage/photos/哈尔滨3.jpg" /></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg" src="http://onx1obrfu.bkt.clouddn.com/joystorage/photos/哈尔滨4.jpg" /></div></li>'+
-            '</ul><br/>';
-    $("#myphoto").html(pages);
-
-     var pages='<ul class="photo_ul">'+
-    		'<h1>湖北·宜昌</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '</ul><br/>';
-    $("#myphoto").append(pages);
-
-     var pages='<ul class="photo_ul">'+
-    		'<h1>北京</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '</ul><br/>';
-    $("#myphoto").append(pages);
-
-     var pages='<ul class="photo_ul">'+
-    		'<h1>广东·深圳</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg" src=" /></div></li>'+
-            '</ul><br/><br/><br/>';
-    $("#myphoto").append(pages);
-
-     var pages='<ul class="photo_ul">'+
-    		'<h1>吉林·长春</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '</ul><br/><br/><br/>';
-    $("#myphoto").append(pages);
-
-        var pages='<ul class="photo_ul">'+
-    		'<h1>厦门</h1><br/><br/><p></p>'+
-			'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-        	'<li class="photo_li"><div class="img-box"><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg"/></div></li>'+
-            '</ul><br/><br/><br/>';
-    $("#myphoto").append(pages);
 
     var pin_height = $(".photo_li").width(); //   获取div的宽度
     $(".photo_li").css('height', pin_height);  //  设置div 
