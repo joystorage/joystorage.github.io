32fa2b689e0fda1aae1b8c6ece2eee3c2d318dee
joyyzhang <joyyzhang>
2017-04-05 11:21:01 +0800

    Site updated: 2017-04-05 11:21:01

--- js/photo.js
+++ js/photo.js
@@ -1,5 +1,17 @@
 $(function(){
 
+	var data = '{"comments":[{"content":"很不错嘛","id":1,"nickname":"纳尼"},{"content":"哟西哟西","id":2,"nickname":"小强"}]}';
+
+	$.getJSON("loadInfo", function(data) {
+        $("#info").html("");//清空info内容
+        $.each(data.comments, function(i, item) {
+            console.log(
+                    "<div>" + item.id + "</div>" + 
+                    "<div>" + item.nickname    + "</div>" +
+                    "<div>" + item.content + "</div><hr/>");
+    	});
+    });
+
     var pages='<ul class="photo_ul">'+
     		'<h1>黑龙江·哈尔滨</h1><br/><br/><p></p>'+
 			'<li class="photo_li"><div class="img-box"><a class="img-bg" rel="example_group" href="http://onx1obrfu.bkt.clouddn.com/joystorage/photos/哈尔滨1.jpg"></a><img class="img-img1 scrollLoading"  data-url="/photos/timg.jpg" src="http://onx1obrfu.bkt.clouddn.com/joystorage/photos/哈尔滨1.jpg" /></div></li>'+
