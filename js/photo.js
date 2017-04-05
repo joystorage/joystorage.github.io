10459e39480ecff5445130278b94ef82a198b29c
joyyzhang <joyyzhang>
2017-04-05 13:38:17 +0800

    Site updated: 2017-04-05 13:38:17

--- js/photo.js
+++ js/photo.js
@@ -62,7 +62,7 @@ $(function(){
 		    	var con = secpath+item2.url;
 		    	pages += '<li class="photo_li_mob">'+
 		    			'<div class="img-box"><a href="'+con+'">'+
-		    			'<img class="img-img2"  data-url="/photos/timg.jpg" src="'+con+'" /></div></li></a>';
+		    			'<img class="img-img2 scrollLoading"  data-url="/photos/timg.jpg" src="'+con+'" /></div></li></a>';
 			});
 
 			pages += '</ul><br/>';
@@ -74,8 +74,69 @@ $(function(){
 		$(".img-box").css('height', pin_height);  //  设置div  高度
 		$(".img-img2").css('width', pin_height);  //  设置div  高度
 		$(".img-img2").css('height', pin_height);  //  设置div 
+
+		//实现图片慢慢浮现出来的效果
+        $("img").load(function () {
+            //图片默认隐藏  
+            $(this).hide();
+            //使用fadeIn特效  
+            $(this).fadeIn("5000");
+        });
+        // 异步加载图片，实现逐屏加载图片
+        $(".scrollLoading").scrollLoading(); 
+
 		});
  	}
 	
 
 });
+
+(function($) {
+	// alert($.fn.scrollLoading);
+	$.fn.scrollLoading = function(options) {
+		var defaults = {
+			attr: "data-url"
+		};
+		var params = $.extend({}, defaults, options || {});
+		params.cache = [];
+		$(this).each(function() {
+			var node = this.nodeName.toLowerCase(),
+				url = $(this).attr(params["attr"]);
+			if(!url) {
+				return;
+			}
+			var data = {
+				obj: $(this),
+				tag: node,
+				url: url
+			};
+			params.cache.push(data);
+		});
+
+		var loading = function() {
+			var st = $(window).scrollTop(),
+				sth = st + $(window).height();
+			$.each(params.cache, function(i, data) {
+				var o = data.obj,
+					tag = data.tag,
+					url = data.url;
+				if(o) {
+					post = o.position().top;
+					posb = post + o.height();
+					if((post > st && post < sth) || (posb > st && posb < sth)) {
+						if(tag === "img") {
+							o.attr("src", url);
+						} else {
+							o.load(url);
+						}
+						data.obj = null;
+					}
+				}
+			});
+			return false;
+		};
+
+		loading();
+		$(window).bind("scroll", loading);
+	};
+})(jQuery);
