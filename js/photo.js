149d885129f9d6a8a35f873440a10ee2c624fcaa
joyyzhang <joyyzhang>
2017-04-05 13:28:12 +0800

    Site updated: 2017-04-05 13:28:11

--- js/photo.js
+++ js/photo.js
@@ -1,7 +1,7 @@
 $(function(){
 	 if (!navigator.userAgent.match(/mobile/i)) {//如果是桌面端
 		$("#photo_div").html('');
-		var pages = '<img class="scrollLoading"  data-url="/photos/timg.jpg" '+
+		var pages = '<br/><br/><img class="scrollLoading"  data-url="/photos/timg.jpg" '+
 				'src="http://onx1obrfu.bkt.clouddn.com/joystorage/photos/毕业3 .jpg"/>'+
 				'<br/><br/>';
 		$("#photo_div").append(pages);
@@ -39,7 +39,7 @@ $(function(){
 		});
  	}else{
  		$("#photo_div").html('');
-		var pages = '<img class="scrollLoading"  data-url="/photos/timg.jpg" '+
+		var pages = '<br/><br/><img class="scrollLoading"  data-url="/photos/timg.jpg" '+
 				'src="http://onx1obrfu.bkt.clouddn.com/joystorage/photos/毕业3 .jpg"/>'+
 				'<br/><br/>';
 		$("#photo_div").append(pages);
@@ -62,7 +62,7 @@ $(function(){
 		    	var con = secpath+item2.url;
 		    	pages += '<li class="photo_li_mob">'+
 		    			'<div class="img-box"><a href="'+con+'">'+
-		    			'<img class="img-img1"  data-url="/photos/timg.jpg" lazy-src="'+con+'" /></div></li></a>';
+		    			'<img class="img-img2"  data-url="/photos/timg.jpg" src="'+con+'" /></div></li></a>';
 			});
 
 			pages += '</ul><br/>';
@@ -72,8 +72,8 @@ $(function(){
 		var pin_height = $(".photo_li").width(); //   获取div的宽度
 		$(".photo_li").css('height', pin_height);  //  设置div 
 		$(".img-box").css('height', pin_height);  //  设置div  高度
-		$(".img-img1").css('width', pin_height);  //  设置div  高度
-		$(".img-img1").css('height', pin_height);  //  设置div 
+		$(".img-img2").css('width', pin_height);  //  设置div  高度
+		$(".img-img2").css('height', pin_height);  //  设置div 
 		});
  	}
 	
